f5bb7249ef32e67a5e0b12596734bf3d5f357f57
mohdhassan7890@gmail.com <mohdhassan7890@gmail.com>
2023-09-14 00:20:31 +0530

    first commit

---
+++ WebApplication1/WebApplication1/Data/WebApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApplication1.Data
+{
+    public class WebApplicationDbContext : DbContext
+    {
+        public WebApplicationDbContext (DbContextOptions<WebApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<WebApplication1.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
